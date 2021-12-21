c985f73f33b142fdfc3f3c577feaa06e7f42402c
Connor Hollis <connorhollis3d@gmail.com>
2021-12-20 22:25:06 -0700

    updated port for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
 
@@ -66,3 +68,4 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
