f013c8ecfffa0f4a381600f42285b87b9d952ad9
Connor Hollis <connorhollis3d@gmail.com>
2021-12-22 16:26:17 -0700

    added separate dataset and refactored code

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 // provided a file path to a location in our application (in this case, the public folder) and instruct the server to make these files static resources
 app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
